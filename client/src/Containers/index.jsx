d160c3a55efa07ded191091b7989a4dc88e36bab
Andrew971 <sordier.andrew@gmail.com>
2018-05-03 20:06:48 -0700

    update package json

--- client/src/Containers/index.jsx
+++ client/src/Containers/index.jsx
@@ -6,7 +6,9 @@ import logo from '../Assets/logo.svg';
 class Containers extends Component {
 
   render() {
-    return (<div className="App">
+    return (
+
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">Welcome to React</h1>
@@ -16,7 +18,8 @@ class Containers extends Component {
         <code>src/App.js</code>
         and save to reload.
       </p>
-    </div>);
+    </div>
+);
   }
 }
 const mapStateToProps = (state) => {
