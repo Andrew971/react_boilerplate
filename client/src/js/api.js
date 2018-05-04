7477c892a42747a951a2e33ab31b408bce5dbd3b
Andrew971 <sordier.andrew@gmail.com>
2018-05-03 20:36:13 -0700

    UPDATE Api config

--- client/src/js/api.js
+++ client/src/js/api.js
@@ -1,1 +1,5 @@
-export const apiURL = 'YOUR_URL'
+export const apiURL =(process.env.NODE_ENV ==='production')
+  ?'YOUR_URL_FOR_PROD'
+  :(process.env.NODE_ENV ==='staging')
+  ?'YOUR_URL_FOR_PROD'
+  :'http://localhost:8080/YOUR_END_POINT'
